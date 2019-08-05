79308e6da435699bb0536ebb3c21c059bcc951bc
Justine Robert Igune <justine.igune@stud.kiu.ac.ug>
2019-08-05 14:37:21 +0300

    Area of a Rectangle Calculator

---
+++ script.js
@@ -0,0 +1,13 @@
+// Using variables, calculate the area of a rectangle
+var width = 8;
+// Update the width variable to 7
+width+=7;
+
+var height = 9;
+//Update the height variable to 3
+height-=3;
+
+var area = width * height;
+
+// Print the area variable
+console.log(area);
